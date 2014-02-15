931f79b71ef21a14d0cf6cfeccb8f8faa950a83e
James Mishra <mish0116@umn.edu>
2014-02-15 02:33:28 -0600

    Added debugging to webkit speech API code

--- js/controllers.js
+++ js/controllers.js
@@ -12,21 +12,26 @@ controllers.controller('bespokenSpeech', ['$scope', function($scope)  {
     $scope.recording = false;
     $scope.startRecognition = function () {
         recognition.start();
+        console.log("User started recognition.");
     };
 
     $scope.stopRecognition = function () {
         recognition.stop();
+        console.log("User stopped recognition");
     };
 
     recognition.onstart = function () {
         $scope.recording = true;
+        console.log("Recognition API has been started.");
     };
 
     recognition.onend = function () {
         $scope.recording = false;
+        console.log("Recognition API has been stopped.");
     };
 
     recognition.onerror = function (event) {
+        console.log("There has been an error.");
         console.log(event);
         $scope.recording = false;
         window.alert("there has been an error");
