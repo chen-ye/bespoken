6f6b1c829079ddd1aff8784f5bb70eca6389a382
James Mishra <mish0116@umn.edu>
2014-02-15 01:54:49 -0600

    Fixed a series of AngularJS errors

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,7 @@
 'use strict';
 
 var controllers = angular.module('bespoken.controllers', []);
+
+controllers.controller('bespokenSpeech', ['$scope', function($scope)  {
+    console.log("swag school");
+}]);
