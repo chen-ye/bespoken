0abbcd0f2d61d470dae56b0ae3b6d1365372598a
James Mishra <mish0116@umn.edu>
2014-02-15 00:25:21 -0600

    Added the basic outline for an AngularJS app.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = angular.module('bespoken', [
+    'ngRoute',
+    'ngResource',
+    'bespoken.filters',
+    'bespoken.services',
+    'bespoken.directives',
+    'bespoken.controllers'
+]);
