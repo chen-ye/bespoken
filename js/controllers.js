b4c8b6baa4933267f17a4e8a1b688b151de50a5e
James Mishra <mish0116@umn.edu>
2014-02-15 02:20:54 -0600

    Put the foundation of the Webkit voice api

--- js/controllers.js
+++ js/controllers.js
@@ -4,4 +4,37 @@ var controllers = angular.module('bespoken.controllers', []);
 
 controllers.controller('bespokenSpeech', ['$scope', function($scope)  {
     console.log("swag school");
+    $scope.data = "Initial.";
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    $scope.recording = false;
+    $scope.startRecognition = function () {
+        recognition.start();
+    };
+
+    $scope.stopRecognition = function () {
+        recognition.stop();
+    };
+
+    recognition.onstart = function () {
+        $scope.recording = true;
+    };
+
+    recognition.onend = function () {
+        $scope.recording = false;
+    };
+
+    recognition.onerror = function (event) {
+        console.log(event);
+        $scope.recording = false;
+        window.alert("there has been an error");
+    };
+
+    recognition.onresult = function (event) {
+        console.log("Retrieved result");
+        console.log(event);
+        $scope.data = event;
+    };
 }]);
